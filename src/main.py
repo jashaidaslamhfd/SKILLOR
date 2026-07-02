00383a9cf1be23325cbac58e28d5015861b4d217
jashaidaslamhfd <jamshaidaslamhfd@gmail.com>
2026-07-03 02:43:56 +0500

    Rename video_builder to video_generator

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from script_generator import generate_script
 from image_generator import generate_images
 from voice_generator import generate_voice # gTTS hata diya
 from caption_generator import add_captions
-from video_builder import build_video
+from video_generator import generate_video
 from uploader import upload_all
 
 def main():
